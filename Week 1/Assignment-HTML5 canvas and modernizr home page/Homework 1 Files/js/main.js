362ab1b56151324a43642f1dd2df7755bfe33aa6
AuntieEm <Emily.davis.5828@gmail.com>
2016-05-06 04:15:46 -0500

    WEEK 1 ASSIGNMENT PART 1
    - DONE - Draw a rectangle starting at point (0 ,0)
    - DONE - That has a width of 50 px and a heigh of 100px
    - DONE - Set the color of the rectangle to a shade of blue.
    - DONE - Set the stroke color to black and the dimension of the stroke
    are the same as the rectangle.

--- Week 1/Assignment-HTML5 canvas and modernizr home page/Homework 1 Files/js/main.js
+++ Week 1/Assignment-HTML5 canvas and modernizr home page/Homework 1 Files/js/main.js
@@ -17,10 +17,10 @@ HTML5 Shape Drawing Activity
 FILE SETUP
 
 // - DONE - Setup up 7 different Canvases in index.html one for each problem.
-// - DONE -Link Modernizr.js
+// - DONE - Link Modernizr.js
 // - DONE - Link the main.js file
 // - DONE - Setup the call to that canvas and get it's 2d context
-//Use Modernizr to verify that your browser supports canvas, include a fallback message
+// - DONE - Use Modernizr to verify that your browser supports canvas, include a fallback message
 */
 
 window.onload = function(){
@@ -29,10 +29,10 @@ window.onload = function(){
 /*******************************************
 PART 1
 
-Draw a rectangle starting at point (0 ,0)
-That has a width of 50 px and a heigh of 100px
-Set the color of the rectangle to a shade of blue.
-Set the stroke color to black and the dimension of the stroke are the same as the rectangle.
+- DONE - Draw a rectangle starting at point (0 ,0)
+- DONE - That has a width of 50 px and a heigh of 100px
+- DONE - Set the color of the rectangle to a shade of blue.
+- DONE - Set the stroke color to black and the dimension of the stroke are the same as the rectangle.
 
 Reminder - set the style first then draw.
 ********************************************/
@@ -40,6 +40,19 @@ Reminder - set the style first then draw.
 //Draw Rectangle here
 
 var theCanvas1 = document.getElementById("Canvas1");
+
+if(theCanvas1 && theCanvas1.getContext){
+	var ctx1 = theCanvas1.getContext("2d");
+	if(ctx1){
+		ctx1.strokeStyle = "black";
+		ctx1.fillStyle = "blue";
+		ctx1.lineWidth = 10;
+		ctx1.strokeRect(0,0,50,100);
+		ctx1.fillRect(0,0,50,100);
+		
+		}//closes if ctx1
+	
+	}//closes if part 1
 
 /*******************************************
 PART 2
@@ -56,6 +69,16 @@ Use the arc method
 
 //Draw Circle here
 
+var theCanvas2 = document.getElementById("Canvas2");
+
+if(theCanvas2 && theCanvas2.getContext){
+	var ctx2 = theCanvas2.getContext("2d");
+	if(ctx2){
+		
+		
+		}//closes if ctx2
+	
+	}//closes if part 2
 
 
 
@@ -73,6 +96,17 @@ Height and width and color are up to you.
 
 //Draw Star here
 
+var theCanvas3 = document.getElementById("Canvas3");
+
+if(theCanvas3 && theCanvas3.getContext){
+	var ctx3 = theCanvas3.getContext("2d");
+	if(ctx3){
+		
+		
+		}//closes if ctx3
+	
+	}//closes if part 3
+
 
 /*******************************************
 PART 4
@@ -88,6 +122,17 @@ Do not overlap any other object.
 
 //Draw Umbrella top here
 
+var theCanvas4 = document.getElementById("Canvas4");
+
+if(theCanvas4 && theCanvas4.getContext){
+	var ctx4 = theCanvas2.getContext("2d");
+	if(ctx4){
+		
+		
+		}//closes if ctx4
+	
+	}//closes if part 4
+
 /*******************************************
 PART 5
 
@@ -97,6 +142,17 @@ Draw text into your canvas.  It can said whatever you would like in any color.
 ********************************************/
 
 //Draw text here
+
+var theCanvas5 = document.getElementById("Canvas5");
+
+if(theCanvas5 && theCanvas5.getContext){
+	var ctx5 = theCanvas5.getContext("2d");
+	if(ctx5){
+		
+		
+		}//closes if ctx5
+	
+	}//closes if part 5
 
 /*******************************************
 PART 6
@@ -113,6 +169,17 @@ Reminder to use the drawImage method for all 3 of the ways.
 
 //Draw images here
 
+var theCanvas6 = document.getElementById("Canvas6");
+
+if(theCanvas6 && theCanvas6.getContext){
+	var ctx6 = theCanvas6.getContext("2d");
+	if(ctx6){
+		
+		
+		}//closes if ctx6
+	
+	}//closes if part 6
+
 
 
 /*******************************************
@@ -128,7 +195,16 @@ You must use at least 3 different methods.
 
 // Draw scene here
 
+var theCanvas7 = document.getElementById("Canvas7");
 
+if(theCanvas7 && theCanvas7.getContext){
+	var ctx7 = theCanvas7.getContext("2d");
+	if(ctx7){
+		
+		
+		}//closes if ctx7
+	
+	}//closes if part 7
 
 
 
