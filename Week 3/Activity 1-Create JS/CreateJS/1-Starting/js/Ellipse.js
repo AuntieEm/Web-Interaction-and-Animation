31b6ee34639814a360a8ee1dc2569527a4c14407
AuntieEm <Emily.davis.5828@gmail.com>
2016-05-17 17:01:01 -0500

    Week 3 Activity 1 - CreateJs
    part 5 of 12 - Drawing Ellipses

--- Week 3/Activity 1-Create JS/CreateJS/1-Starting/js/Ellipse.js
+++ Week 3/Activity 1-Create JS/CreateJS/1-Starting/js/Ellipse.js
@@ -1,5 +1,33 @@
 window.onload = function() {
 	
+	//Canvas & Stage
+	var canvas = document.getElementById("canvas1");
+	var stage = new createjs.Stage(canvas);
+	
+	//Create a shape
+	var shape = new createjs.Shape();
+	
+	//Set Stroke
+	shape.graphics.beginStroke("purple");
+	shape.graphics.setStrokeStyle(10,0,1);
+	
+	//Draw circle
+	//drawCircle(x,y,radius)
+	shape.graphics.drawCircle(100,100,50);
+	
+	//draw ellipse
+	//drawEllipse(x,y,w,h)
+	shape.graphics.drawEllipse(75, 200, 150, 50);
+	
+	//draw arcs
+	//arc(x,y,radius,startAngle,endAngle,anticlockwise = 1 clockwise*=0)
+	//Angles are in radians!
+	shape.graphics.beginStroke("red");
+	shape.graphics.arc(100,100,75,0,-90*(Math.PI/180),1);
+	
+	
+	stage.addChild(shape);
+	stage.update();
 	
 
 }
