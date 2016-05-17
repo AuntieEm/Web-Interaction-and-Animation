5c1b83a9dd13482750b30e6676c04e072989fb18
AuntieEm <Emily.davis.5828@gmail.com>
2016-05-17 15:15:50 -0500

    Week 2 Activity 4 - jQuery Transitions
    part 4 of 5 - Animating an element

--- Week 2/Activity 4-jQuery Transitions/jQuery/1-Starting/js/Animate.js
+++ Week 2/Activity 4-jQuery Transitions/jQuery/1-Starting/js/Animate.js
@@ -1,4 +1,35 @@
 $(function() { 
+	$("#right").click(function(){
+		$("#theDiv").animate({width: "500px"}, 1000);
+		
+		});
+		
+	$("#text").click(function(){
+		
+			$("#theDiv").animate({fontSize:"24pt"}, 1000);
+		
+		});
 	
+	$("#move").click(function(){
+		$("#theDiv").animate({left:"500"}, 1000, "swing");
+		
+		});
+		
+	$("#multiple").click(function(){
+		
+		//Animates all at once
+		//$("#theDiv").animate({height: "500px", width:"500px", left:"500", fontSize:"24pt"}, 1000, "swing");
+		$("#theDiv").animate({height:"500px"}, 1000, "swing");
+		$("#theDiv").animate({width:"500px"}, 1000, "swing");
+		$("#theDiv").animate({left:"500"}, 1000, "swing");
+		$("#theDiv").animate({fontSize:"24pt"}, 1000, "swing");
+		
+		});
+		
+	$("#reset").click(function(){
+		//Reset to all the initial values
+		$("#theDiv").animate({height:"180px", width:"250px", left:"0", fontSize:"16pt"}, 1000, "swing");
+		
+		});
 
 });
