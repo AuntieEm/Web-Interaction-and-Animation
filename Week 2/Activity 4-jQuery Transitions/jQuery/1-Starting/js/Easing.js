986b463182550a0a8ebdedb036c7efba7e493854
AuntieEm <Emily.davis.5828@gmail.com>
2016-05-17 15:29:27 -0500

    Week 2 Activity 4 - jQuery Transitions
    part 5 of 5 - Easings

--- Week 2/Activity 4-jQuery Transitions/jQuery/1-Starting/js/Easing.js
+++ Week 2/Activity 4-jQuery Transitions/jQuery/1-Starting/js/Easing.js
@@ -1,24 +1,18 @@
 $(function() { 
-
-            $("#right").click(function() {
-                $("#theDiv").animate({ width: "500px" }, 1000, "swing");
-            });
-
-            $("#text").click(function() {
-                $("#theDiv").animate({ fontSize: "24pt" }, 1000, "swing");
-            });
-
-            $("#move").click(function() {
-                $("#theDiv").animate({ left: "500" }, 1000, "swing");
-            });
-            
-            $("#multiple").click(function() {
-                $("#theDiv").animate({height: "500px", width: "500px", left: "500", fontSize: "24pt" }, 1000, "linear");
-            });
-
-            $("#reset").click(function() {
-                $("#theDiv").animate({ height: "180px", width: "250px", left: "0", fontSize: "16pt" }, 1000, "linear");
-             }); 
-					
+	$("#right").click(function(){
+		$("#theDiv").animate({width:"500px"}, 1000, "easeInCubic");
+		});
+	$("#text").click(function(){
+		$("#theDiv").animate({fontSize:"24pt"}, 1000, "easeInOutElastic");
+		});
+	$("#move").click(function(){
+		$("#theDiv").animate({left:"500"}, 1000, "easeOutBounce");
+		});
+	$("#multiple").click(function(){
+		$("#theDiv").animate({height:"500px", width:"500px", left:"500", fontSize:"24pt"}, 1000, "easeInBack");
+		});	
+	$("#reset").click(function(){
+		$("#theDiv").animate({height:"180px", width:"250px", left:"0", fontSize:"16pt"}, 1000, "easeInOutBack");
+		});
  
 });
